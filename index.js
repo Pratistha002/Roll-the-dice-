a6b256b3039624c65f1e48b092c2342ace9b42c9
Pratistha Singh <98650032+Pratistha002@users.noreply.github.com>
2024-09-13 00:19:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1 = Math.floor( Math.random() *6) +1 ;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+randomImageSource =  randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+if ( randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = " Player 1 is Winner 🚩";
+
+}
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 is Winner🚩 ";
+
+}
+
+else{
+    document.querySelector("h1").innerHTML = " Match Draw! 😑";
+}
